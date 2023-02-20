98f2f199e0a365d3c3e4e789b0b1eb711aa8efc9
Michael Gastello <michaelgastello@aol.com>
2023-02-20 17:25:28 -0500

    done for the day

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,37 @@
 class User < ApplicationRecord
-
-
-
     validates :username, :session_token, :password_digest, presence: true
     validates :username, :session_token, uniqueness: true
+    validates :password, length: { minimum: 6 } allow_nil: true
 
     before_validation :ensure_session_token
 
+    attr_reader :password
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(password_digest).is_password?(password)
+    end
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def reset_session_token! ##
+        self.session_token = generate_unique_random_token
+        self.save!
+        self.session_token
+    end
+
+    private
 
     def generate_unique_random_token
         token = SecureRandom::urlsafe_base64
@@ -15,15 +40,7 @@ class User < ApplicationRecord
         end
     end
 
-
     def ensure_session_token
         self.session_token ||= generate_unique_random_token
     end
-
-    
-
-
-
-
-
 end
